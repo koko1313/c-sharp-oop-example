db74f4b678fcb36c37513d4f05dbe9c5ef9c8a90
koko1313 <kaloyanvellichkov@gmail.com>
2019-12-10 00:51:43 +0200

    class human created

---
+++ Human.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_sharp_oop_example
+{
+    class Human
+    {
+        String name;
+
+        public Human(string name)
+        {
+            this.name = name;
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("I am human. I will start waling right now! Also my name is " + name);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,10 +9,12 @@ namespace c_sharp_oop_example
             Car car1 = new Car(5, 2500, 4, 5);
             Bicycle bicycle1 = new Bicycle(1, 10, 2, "Red");
             Airplane airplane1 = new Airplane(500, 50000, 2, 2);
+            Human human1 = new Human("Ivan");
 
             StartMoving(car1);
             StartMoving(bicycle1);
             StartMoving(airplane1);
+            // StartMoving(human1);
 
             void StartMoving(Vehicle movableThing)
             {
